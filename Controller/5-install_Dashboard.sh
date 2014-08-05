5c76b02ab3a7fec50c909654c6637b31bf8a4101
luongvinhthao <luongvinhthao@gmail.com>
2014-08-05 13:57:32 +0700

    add -y

--- Controller/5-install_Dashboard.sh
+++ Controller/5-install_Dashboard.sh
@@ -5,7 +5,7 @@ set -e
 echo "---------------------------- Install Dashboard ----------------------------"
 apt-get install -y  apache2 memcached libapache2-mod-wsgi openstack-dashboard
 
-apt-get remove --purge openstack-dashboard-ubuntu-theme
+apt-get remove --purge openstack-dashboard-ubuntu-theme -y
 
 
 sed -i 's|OPENSTACK_HOST = "127.0.0.1"|OPENSTACK_HOST = "'$HOST_NAME'"|' /etc/openstack-dashboard/local_settings.py

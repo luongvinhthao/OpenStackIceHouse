f6f16664dcea8ee219b0a0c821e5bcaaedd586da
luongvinhthao <luongvinhthao@gmail.com>
2014-08-05 13:52:58 +0700

    remove nova-api.conf

--- Controller/4-install_Nova.sh
+++ Controller/4-install_Nova.sh
@@ -7,14 +7,15 @@ apt-get install -y nova-novncproxy novnc nova-api \
   nova-ajax-console-proxy nova-cert nova-conductor \
   nova-consoleauth nova-doc nova-scheduler \
   python-novaclient
-if [ !-f /etc/nova/nova.conf.bak ]; then #check the file exist 
+if [ ! -f /etc/nova/nova.conf.bak ]; then #check the file exist 
 	#statements
-	echo "back up file config"
+	echo "back up file nova.conf"
 	cp /etc/nova/nova.conf /etc/nova/nova.conf.bak
 fi
 
 echo "---------------------------- Edit nova.conf ----------------------------"
 cat << EOF > /etc/nova/nova.conf
+
 [DEFAULT]
 dhcpbridge_flagfile=/etc/nova/nova.conf
 dhcpbridge=/usr/bin/nova-dhcpbridge
@@ -39,15 +40,15 @@ admin_tenant_name = service
 admin_user = nova
 admin_password = $NOVA_PASS
 
-my_ip=$HOST_IP
-vncserver_listen=$HOST_IP
-vncserver_proxyclient_address=$HOST_IP
+my_ip = $HOST_IP
+vncserver_listen = $HOST_IP
+vncserver_proxyclient_address = $HOST_IP
 
 rpc_backend = nova.rpc.impl_kombu
 rabbit_host = $HOST_NAME
 rabbit_password = $RABBIT_PASS
 
-auth_strategy=keystone
+auth_strategy = keystone
 
 [database]
 connection = mysql://nova:$NOVA_DBPASS@$HOST_NAME/nova
@@ -65,17 +66,9 @@ sleep 5
 echo "---------------------------- Edit nova api-paste.ini  ----------------------------"
 if [ -f /etc/nova/api-paste.ini ]; then #check the file exist 
 	#statements
-	echo "back up file config"
+	echo "back up file api-paste.ini"
 	cp /etc/nova/api-paste.ini /etc/nova/api-paste.ini.bak
 fi
-
-sed -i '/auth_host = 127.0.0.1/aauth_uri = http://HOST_NAME:5000/' /etc/nova/api-paste.ini && \
-sed -i 's|HOST_NAME|'$HOST_NAME'|' /etc/nova/api-paste.ini
-
-sed -i 's|auth_host = 127.0.0.1|auth_host = '$HOST_NAME'|' /etc/nova/api-paste.ini 
-sed -i 's|admin_tenant_name = %SERVICE_TENANT_NAME%|admin_tenant_name = '$SERVICE_TENANT_NAME'|'  /etc/nova/api-paste.ini
-sed -i 's|admin_user = %SERVICE_USER%|admin_user = nova|' /etc/nova/api-paste.ini
-sed -i 's|admin_password = %SERVICE_PASSWORD%|admin_password = '$NOVA_PASS'|' /etc/nova/api-paste.ini
 
 
 echo "---------------------------- Restart nova service  ----------------------------"

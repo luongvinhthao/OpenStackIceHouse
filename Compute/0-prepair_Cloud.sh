fb2234e0cf797fb78bfc683102dd76ab756de980
luongvinhthao <luongvinhthao@gmail.com>
2014-08-05 13:44:03 +0700

    add new file

---
+++ Compute/0-prepair_Cloud.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash 
+source config.cnf
+set -e 
+
+
+echo "---------------------------- Update ubuntu package ----------------------------"
+
+apt-get install -y python-software-properties && add-apt-repository cloud-archive:icehouse -y
+apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
+
+echo "---------------------------- Config hostname on ubuntu ----------------------------"
+hostname $HOST_NAME
+echo "$HOST_NAME" > /etc/hostname
+
+echo "---------------------------- Install and config NTP ----------------------------"
+apt-get install -y ntp
+
+echo "---------------------------- Restart NTP ----------------------------"
+sleep 3
+service ntp restart
+echo "---------------------------- Install Messaging server cho ubuntu ----------------------------"
+apt-get install -y rabbitmq-server
+echo "$RABBIT_PASS"
+rabbitmqctl change_password guest $RABBIT_PASS
+
+echo "---------------------------- Restart Rabbit and Finish ----------------------------"
+sleep 2
+service rabbitmq-server restart
+sleep 2
+init 6
+echo "---------------------------- Finish ----------------------------"
+
+ 
